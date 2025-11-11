54be497a65a7133b62ee9857b71393ba112fbe51
Advay Singh <Advay@umich.edu>
2025-11-11 16:56:30 -0500

    added CMatrix and AMatrix init. TODO: refactor, convert to double, create BVector, and validate

--- QPEngine.cpp
+++ QPEngine.cpp
@@ -48,14 +48,9 @@ class QPEngine {
     QPEngine() noexcept = default;
     QPEngine(size_t recCount) noexcept : recCount_(recCount) {}
 
-    /**
-     * @brief Consider making the following types private
-     */
+    /* public typdefs */
     using matrix_t = std::vector<std::vector<size_t>>; 
     using coordinate_t = std::pair<size_t, size_t>; 
-
-    // TODO: make these types private:
-    using coordinateList_t = std::vector<coordinate_t>;
 
     /**
      * @brief Main driver function.
@@ -76,13 +71,17 @@ class QPEngine {
     static void printMatrix(const matrix_t& m);
 
     private:
+    /* private types */
+    using coordinateList_t = std::vector<coordinate_t>;
+    using netList_t = std::vector<std::pair<std::vector<size_t>, std::vector<size_t>>>;
     /* helper functions */
     /**
-     * @brief Read the netlist into the cMatrix from the file
+     * @brief Read the netlist into netToGateAndPortListMap
+     * Also fills in portToCoordinateMap_
      * 
      * @param f 
      */
-    void readNetlist(std::ifstream& inFile);
+    netList_t _readNetlist(std::ifstream& inFile);
 
     /**
      * @brief Construct a new check Bounds object
@@ -91,14 +90,38 @@ class QPEngine {
      * @param bound 
      * @param msg 
      */
-    void inline checkBounds(const size_t val, const size_t bound, const std::string& msg) const;
+    void inline _checkBounds(const size_t val, const size_t bound, const std::string& msg) const;
+
+    /**
+     * @brief Given a netList_t map, creates a thing cMatrix
+     * Note: input files in this netlist don't have weights
+     * specifed, hence, they are all 0
+     * 
+     * @param netToGateAndPortListMap 
+     * @return matrix_t 
+     */
+    [[nodiscard]] matrix_t _createCMatrix(const netList_t& netToGateAndPortListMap) const noexcept;
+
+
+    [[nodiscard]] matrix_t _createAMatrix(const matrix_t& c, const netList_t& netToGateAndPortListMap) const;
+
+
+    /**
+     * @brief Given netList_t map, returns number of gates (cells)
+     * in the netlist
+     * 
+     * @param netToGateAndPortListMap 
+     * @return size_t 
+     */
+    [[nodiscard]] size_t inline _getNumGates(const netList_t& netToGateAndPortListMap) const noexcept;
+
 
 
 
     /* member variables */
 
     // matrix representing the connections
-    matrix_t cMatrix = matrix_t();
+    // matrix_t cMatrix = matrix_t();
     // port to coordinate map for the external ports
     coordinateList_t portToCoordinateMap_ = coordinateList_t();
     // number of recursions which the placer does
@@ -134,7 +157,7 @@ int main(int argc, char** argv) {
 
 // TODO: move these functions onto a different file
 
-void QPEngine::checkBounds(const size_t val, const size_t bound, const std::string& msg) const {
+void QPEngine::_checkBounds(const size_t val, const size_t bound, const std::string& msg) const {
   if (val >= bound) {
     throw std::out_of_range(msg);
   }
@@ -142,22 +165,21 @@ void QPEngine::checkBounds(const size_t val, const size_t bound, const std::stri
 
 
 void QPEngine::run(std::ifstream& inFile, std::ofstream& outFile) {
-  readNetlist(inFile);
+  _readNetlist(inFile);
 }
 
-void QPEngine::readNetlist(std::ifstream& inFile) {
+typename QPEngine::netList_t QPEngine::_readNetlist(std::ifstream& inFile) {
   // read num gates and nets
   size_t numGates, numNets;
   inFile >> numGates >> numNets;
 
   // init numGates * numGates matrix all to 0
-  cMatrix = matrix_t(numGates, std::vector<size_t>(numGates, 0));
+  // cMatrix = matrix_t(numGates, std::vector<size_t>(numGates, 0));
 
   // init an unordered map
   // key: net, value: pair with a vector for gates attached to it
   // and a vector for ports attached to it
-  std::vector<std::pair<std::vector<size_t>, 
-    std::vector<size_t>>> netToGateAndPortListMap(
+  netList_t netToGateAndPortListMap = netList_t(
       numNets,
       {std::vector<size_t>(numGates, 0),
       std::vector<size_t>()}
@@ -174,7 +196,7 @@ void QPEngine::readNetlist(std::ifstream& inFile) {
       size_t gate, numTmpNets, net;
       // reading a gate to gate connection
       ss >> gate;
-      checkBounds(gate, numGates, "Input gate greater than number of gates");
+      _checkBounds(gate, numGates, "Input gate greater than number of gates");
       // read port line
       if (ss.eof()) {
         readPorts = true;
@@ -192,7 +214,7 @@ void QPEngine::readNetlist(std::ifstream& inFile) {
       ss >> numTmpNets; // not really needed?
       // insert net gate mapping into netToGateListMap
       while (ss >> net) {
-        checkBounds(net, numNets, "Input net greater than number of nets");
+        _checkBounds(net, numNets, "Input net greater than number of nets");
         // the weight for these is assumed to be 1
         netToGateAndPortListMap[net].first[gate] = 1;
       }
@@ -200,10 +222,56 @@ void QPEngine::readNetlist(std::ifstream& inFile) {
       size_t port, net, x, y;
       // reading a port to gate connection
       ss >> port >> net >> x >> y;
-      checkBounds(port, numPorts, "Input port greater than number of ports");
-      checkBounds(net, numNets, "Input net greater than number of nets");
+      _checkBounds(port, numPorts, "Input port greater than number of ports");
+      _checkBounds(net, numNets, "Input net greater than number of nets");
       netToGateAndPortListMap[net].second[port] = 1;
       portToCoordinateMap_[port] = std::make_pair(x, y);
     }
   }
-}
+} // QPEngine::readNetlist()
+
+[[nodiscard]] typename QPEngine::matrix_t QPEngine::_createCMatrix(const QPEngine::netList_t& netlist) const noexcept {
+  size_t numGates = _getNumGates(netlist);
+  // create the cmatrix by determing where the connections exist
+  matrix_t c(numGates, std::vector<size_t>(numGates, 0));
+  FOR_EACH(netlist,
+    [](auto& p) {
+      const auto& gates = p.first; 
+      for (size_t i = 0; i < gates.size(); ++i) {
+        for (size_t j = i+1; j < gates.size(); ++j) {
+          c[gates[i]][gates[j]] = c[gates[j]][gates[i]] = 1 / netlist.size();
+        }
+      }
+    });
+    return c;
+  } // QPEngine::_createCMatrix()
+  
+  
+  [[nodiscard]] typename QPEngine::matrix_t 
+  QPEngine::_createAMatrix(const QPEngine::matrix_t& c, const QPEngine::netList_t& netlist) const {
+    size_t numGates = _getNumGates(netlist);
+    if (numGates > c.size() || numGates > c[0].size()) throw std::runtime_error("Matrix c size too small");
+    matrix_t m(numGates, std::vector<size_t>(numGates, 0));
+    for (size_t i = 0; i < numGates; ++i) {
+      for (size_t j = 0; j < numGates; ++j) {
+        if (i == j) {
+          // in the diagonal, sum up the pad (port) wires
+          int portWireSum;
+          FOR_EACH(netlist, 
+            [&portWireSum](const auto& p){
+              if (p.first[i]) {
+                FOR_EACH(p.second, 
+                  [&portWireSum](const auto port) {
+                    if (port) ++portWireSum;
+                  }
+                );
+              }
+            }
+          );
+        } else {
+          // not on the diagonal, m[i][j] = -c[i][j]
+          m[i][j] = -c[i][j];
+        }
+      }
+    }
+  } // QPEngine::_createAMatrix()
